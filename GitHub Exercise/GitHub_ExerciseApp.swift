113d534ab82bdb3f9825ceccdf20480bb60ad7f6
anorazhu <zhusm@bc.edu>
2024-09-02 19:56:20 -0400

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Anora Zhu on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHub ExerciseTests/GitHub_ExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseTests.swift
+//  GitHub ExerciseTests
+//
+//  Created by Anora Zhu on 9/2/24.
+//
+
+import Testing
+@testable import GitHub_Exercise
+
+struct GitHub_ExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
